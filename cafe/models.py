bff293914b1acbf70a9e822bacc1f2207ba0e6ab
Roman <4more52@gmail.com>
2025-02-27 19:06:40 +0300

    create model Order

--- cafe/models.py
+++ cafe/models.py
@@ -6,11 +6,11 @@ class Order(models.Model):
         ('ready', 'Готово'),
         ('paid', 'Оплачено')
     ]
-    table_number = models.IntegerField(related_name='Номер столика')
+    table_number = models.IntegerField(verbose_name='Номер столика')
     items = models.JSONField()
-    total_price = models.DecimalField(max_digits=10, decimal_places=2, related_name='Сумма заказа')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма заказа')
     status = models.CharField(max_length=10, choices=STATUS, default='В ожидании')
-    created_at = models.DateField(auto_now_add=True, related_name='Дата создания')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
 
     def __str__(self):
         return f'Заказ №{self.id}'
