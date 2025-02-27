c1af9767adc0bc1f52dd228c04b599c3a58b7a76
Roman <4more52@gmail.com>
2025-02-27 21:37:44 +0300

    update calculate_revenue in views.py

--- cafe/views.py
+++ cafe/views.py
@@ -1,11 +1,25 @@
+from django.db.models import Sum
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Order
 from .forms import OrderForm
+from django.utils import timezone
 
 
 def order_list(request):
+    query = request.GET.get('query')
+    status_filter = request.GET.get('status')
+
     orders = Order.objects.all()
-    return render(request, 'order_list.html', {'orders': orders})
+    if query:
+        orders = orders.filter(table_number__icontains=query)
+    if status_filter:
+        orders = orders.filter(status=status_filter)
+    status_choices = Order.STATUS
+
+    return render(request, 'order_list.html', {
+        'orders': orders,
+        'status_choices': status_choices,
+    })
 
 
 def add_order(request):
@@ -13,7 +27,6 @@ def add_order(request):
         form = OrderForm(request.POST)
         if form.is_valid():
             order = form.save(commit=False)
-            order.total_price = sum(item['price'] for item in order.items)
             order.save()
             return redirect('order_list')
     else:
@@ -42,5 +55,13 @@ def delete_order(request, order_id):
 
 
 def calculate_revenue(request):
-    total_revenue = Order.objects.filter(status='paid').aggregate(total=models.Sum('total_price'))['total'] or 0
-    return render(request, 'revenue.html', {'total_revenue': total_revenue})
+    today = timezone.now().date()
+    orders_today = Order.objects.filter(
+        status='paid',
+        completed_at__date=today
+    )
+    total_revenue = orders_today.aggregate(total=Sum('total_price'))['total'] or 0
+    return render(request, 'revenue.html', {
+        'total_revenue': total_revenue,
+        'today': today,
+    })
